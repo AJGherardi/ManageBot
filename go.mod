b77f6649fa07343df6ada578b0a7eff4417f811a
Alexander Gherardi <alexander.2003@live.com>
2021-01-17 16:04:08 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AJGherardi/ManageBot
+
+go 1.15
+
+require github.com/bwmarrin/discordgo v0.22.1 // indirect
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/bwmarrin/discordgo"
+
+func main() {
+	discord, _ := discordgo.New("Bot " + "authentication token")
+}
