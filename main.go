cbc18b049662056a5e6a7aa8b828663e2865f49b
Alexander Gherardi <alexander.2003@live.com>
2021-01-17 19:38:34 -0500

    Add info

--- main.go
+++ main.go
@@ -1,61 +1,65 @@
 package main
 
 import (
-	"log"
-
 	dgo "github.com/bwmarrin/discordgo"
 )
 
 var (
-	botToken = "ODAwNDkxNTIzNzYxNTA0Mjg2.YAS50w.FvJlTQtvp0DbP4_mUxUbbOadvAY"
-	guild    = "799794515443318814"
+	botToken = "ODAwNDkxNTIzNzYxNTA0Mjg2.YAS50w.pIKeG2AYPWdHW9Gzyb3V82Gh2iA"
+	guildID  = "799794515443318814"
 )
 
+// Execution starts here
 func main() {
+	// Creates a new client object
 	client, _ := dgo.New("Bot " + botToken)
-
+	// Regesters a event handeler for when the command is called
 	client.AddHandler(func(s *dgo.Session, i *dgo.InteractionCreate) {
+		// Makes a reaponse
 		responseData := &dgo.InteractionApplicationCommandResponseData{
 			TTS:     false,
 			Content: i.Interaction.Data.Options[0].Value.(string),
 		}
+		// Sends the response
 		s.InteractionRespond(i.Interaction, &dgo.InteractionResponse{
 			Type: dgo.InteractionResponseChannelMessage,
 			Data: responseData,
 		})
 	})
-
-	if err := client.Open(); err != nil {
-		log.Fatal(err)
-	}
-
-	client.ApplicationCommandCreate("", &dgo.ApplicationCommand{
-		Name:        "order",
-		Description: "pls input your order",
-		Options: []*dgo.ApplicationCommandOption{
-			{
-				Type:        dgo.ApplicationCommandOptionString,
-				Name:        "Choices",
-				Description: "posible orders",
-				Choices: []*dgo.ApplicationCommandOptionChoice{
-					{
-						Name:  "Food",
-						Value: "Food",
+	// Opens the connection
+	client.Open()
+	// Regesters the command
+	client.ApplicationCommandCreate(
+		"",
+		&dgo.ApplicationCommand{
+			Name:        "order",
+			Description: "pls input your order",
+			Options: []*dgo.ApplicationCommandOption{
+				{
+					Type:        dgo.ApplicationCommandOptionString,
+					Name:        "Choices",
+					Description: "possible orders",
+					Choices: []*dgo.ApplicationCommandOptionChoice{
+						{
+							Name:  "Food",
+							Value: "Food",
+						},
+						{
+							Name:  "Drink",
+							Value: "Drink",
+						},
 					},
-					{
-						Name:  "Drink",
-						Value: "Drink",
-					},
+					Required: true,
 				},
-				Required: true,
 			},
 		},
-	}, guild)
-
+		guildID,
+	)
+	// Keep the app runing
 	for {
 
 	}
-	if err := client.Close(); err != nil {
-		log.Fatal(err)
-	}
-}
+	// if err := client.Close(); err != nil {
+	// 	log.Fatal(err)
+	// }
+} // Execution ends here
