ef0892013aef0deec494b265d32de8ad561c2908
Alexander Gherardi <alexander.2003@live.com>
2021-01-17 21:24:20 -0500

    Improve test

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"time"
+
 	dgo "github.com/bwmarrin/discordgo"
 )
 
 var (
-	botToken = "ODAwNDkxNTIzNzYxNTA0Mjg2.YAS50w.pIKeG2AYPWdHW9Gzyb3V82Gh2iA"
+	botToken = "ODAwNDkxNTIzNzYxNTA0Mjg2.YAS50w.NPCoNQslj3u-fPlIpEp_rQiEFGE"
 	guildID  = "799794515443318814"
 )
 
@@ -18,35 +20,71 @@ func main() {
 		// Makes a reaponse
 		responseData := &dgo.InteractionApplicationCommandResponseData{
 			TTS:     false,
-			Content: i.Interaction.Data.Options[0].Value.(string),
+			Content: "Warning",
 		}
-		// Sends the response
+		// Sends the inital response
 		s.InteractionRespond(i.Interaction, &dgo.InteractionResponse{
 			Type: dgo.InteractionResponseChannelMessage,
 			Data: responseData,
 		})
+		time.Sleep(1 * time.Second)
+		// Get user from parms
+		userID := i.Interaction.Data.Options[0].Value.(string)
+		user, _ := client.User(userID)
+		s.InteractionResponseEdit("", i.Interaction, &dgo.WebhookEdit{
+			Content: "**" + user.Username + "** This is you final warning for " + i.Interaction.Data.Options[1].Value.(string),
+		})
 	})
 	// Opens the connection
 	client.Open()
+	// Remove all commands
+	deleteAllCommands(client)
 	// Regesters the command
 	client.ApplicationCommandCreate(
 		"",
 		&dgo.ApplicationCommand{
-			Name:        "order",
-			Description: "pls input your order",
+			Name:        "Warn",
+			Description: "Warn for user rule violation",
+
 			Options: []*dgo.ApplicationCommandOption{
 				{
+					Type:        dgo.ApplicationCommandOptionUser,
+					Name:        "User",
+					Description: "User to warn",
+					Required:    true,
+				},
+				{
 					Type:        dgo.ApplicationCommandOptionString,
-					Name:        "Choices",
-					Description: "possible orders",
+					Name:        "Violation",
+					Description: "Rules violated",
 					Choices: []*dgo.ApplicationCommandOptionChoice{
 						{
-							Name:  "Food",
-							Value: "Food",
+							Name:  "Gore",
+							Value: "Gore",
 						},
 						{
-							Name:  "Drink",
-							Value: "Drink",
+							Name:  "Harassment",
+							Value: "Harassment",
+						},
+						{
+							Name:  "Disrespecting staff",
+							Value: "Disrespecting staff",
+						},
+						{
+							Name:  "Sexually explicit content",
+							Value: "Sexually explicit content",
+						},
+						{
+							Name:  "Advertizing",
+							Value: "Advertizing",
+						},
+						{
+							Name:  "Spam",
+							Value: "Spam",
+						},
+						{
+							Name:  "Obsessive pinging",
+							Value: "Obsessive Pinging",
 						},
 					},
 					Required: true,
@@ -63,3 +101,18 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 } // Execution ends here
+
+func deleteAllCommands(client *dgo.Session) {
+	apps, _ := client.Applications()
+	for _, app := range apps {
+		cmds, _ := client.ApplicationCommands(app.ID, guildID)
+		for _, cmd := range cmds {
+			client.ApplicationCommandDelete(cmd.ApplicationID, cmd.ID, guildID)
+		}
+
+	}
+	cmds, _ := client.ApplicationCommands("", guildID)
+	for _, cmd := range cmds {
+		client.ApplicationCommandDelete(cmd.ApplicationID, cmd.ID, guildID)
+	}
+}
