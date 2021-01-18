06915b680c8d6354d6f7fa1e4e2feb1ae0f5f6ca
Alexander Gherardi <alexander.2003@live.com>
2021-01-17 19:11:55 -0500

    add test

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/AJGherardi/ManageBot
 
 go 1.15
 
-require github.com/bwmarrin/discordgo v0.22.1 // indirect
+require (
+	github.com/bwmarrin/discordgo v0.22.1
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78 // indirect
+)
+
+replace github.com/bwmarrin/discordgo => github.com/FedorLap2006/discordgo v0.22.1-0.20210117175520-9c6bf001db7e
--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"log"
+
+	dgo "github.com/bwmarrin/discordgo"
+)
+
+var (
+	botToken = "ODAwNDkxNTIzNzYxNTA0Mjg2.YAS50w.FvJlTQtvp0DbP4_mUxUbbOadvAY"
+	guild    = "799794515443318814"
+)
 
 func main() {
-	discord, _ := discordgo.New("Bot " + "authentication token")
+	client, _ := dgo.New("Bot " + botToken)
+
+	client.AddHandler(func(s *dgo.Session, i *dgo.InteractionCreate) {
+		responseData := &dgo.InteractionApplicationCommandResponseData{
+			TTS:     false,
+			Content: i.Interaction.Data.Options[0].Value.(string),
+		}
+		s.InteractionRespond(i.Interaction, &dgo.InteractionResponse{
+			Type: dgo.InteractionResponseChannelMessage,
+			Data: responseData,
+		})
+	})
+
+	if err := client.Open(); err != nil {
+		log.Fatal(err)
+	}
+
+	client.ApplicationCommandCreate("", &dgo.ApplicationCommand{
+		Name:        "order",
+		Description: "pls input your order",
+		Options: []*dgo.ApplicationCommandOption{
+			{
+				Type:        dgo.ApplicationCommandOptionString,
+				Name:        "Choices",
+				Description: "posible orders",
+				Choices: []*dgo.ApplicationCommandOptionChoice{
+					{
+						Name:  "Food",
+						Value: "Food",
+					},
+					{
+						Name:  "Drink",
+						Value: "Drink",
+					},
+				},
+				Required: true,
+			},
+		},
+	}, guild)
+
+	for {
+
+	}
+	if err := client.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
