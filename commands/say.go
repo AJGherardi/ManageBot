7a5f7da04159d05f5e1b85216c6b91ca60ba5644
Eric Lottman <rick821044@gmail.com>
2021-01-19 21:37:30 -0500

    Added handle comment

--- commands/say.go
+++ commands/say.go
@@ -5,6 +5,7 @@ import (
 	dgo "github.com/bwmarrin/discordgo"
 )
 
+// HandleSay handels a say command
 func HandleSay(message string, i *dgo.InteractionCreate, s *dgo.Session) {
 	utils.SendResponse(message, i, s)
 
