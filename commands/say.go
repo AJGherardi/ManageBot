4a61ec6a5e92be0854f7590e75898e87b83796b2
Eric Lottman <rick821044@gmail.com>
2021-01-19 21:38:28 -0500

    Corrected typo

--- commands/say.go
+++ commands/say.go
@@ -5,7 +5,7 @@ import (
 	dgo "github.com/bwmarrin/discordgo"
 )
 
-// HandleSay handels a say command
+// HandleSay handles a say command
 func HandleSay(message string, i *dgo.InteractionCreate, s *dgo.Session) {
 	utils.SendResponse(message, i, s)
 
