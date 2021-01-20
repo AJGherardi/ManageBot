975ad3aed34404eee91956a7b1302eb9da950f84
Eric Lottman <rick821044@gmail.com>
2021-01-19 22:09:40 -0500

    Removed blank lines

--- commands/say.go
+++ commands/say.go
@@ -7,9 +7,7 @@ import (
 
 // HandleSay handles a say command
 func HandleSay(message string, number float64, i *dgo.InteractionCreate, s *dgo.Session) {
-
 	for r := 0; r < int(number); r++ {
 		utils.SendResponse(message, i, s)
 	}
-
 }
