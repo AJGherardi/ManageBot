b882d9203261f7b5698fa3cd3aae14cfda0ed912
Eric Lottman <rick821044@gmail.com>
2021-01-21 18:14:05 -0500

    Added bots and members to stats

--- commands/stats.go
+++ commands/stats.go
@@ -9,32 +9,16 @@ import (
 
 // HandleStats handles a stat command
 func HandleStats(i *dgo.InteractionCreate, s *dgo.Session) {
-	// Gathers stats
-	// Gathers number of members
-		MemberCount int `json:"member_count"`
-	// Gathers numbers of people with staff role
-		numberOfStaff := 0
-	// Gathers numbers of people with staff role that are online
-		numberOfStaffOnline := 0
-		//if /*has role staff*/ && /*is online*/ && /*!marked searched for staff*/{
-			//find some way to mark off that user
-			//numberOfStaffOnline ++
-			//}
-			//else /*skip user*/
-	// Finds the current member amount online
-		ApproximatePresenceCount int `json:"approximate_presence_count"`
-	// Finds number of current boosters
-		PremiumSubscriptionCount int `json:"premium_subscription_count"`
-	// Finds number of bots
-		numberOfBots := 0
-		//if /*is bot*/ && /*!marked searched for bot*/{
-			//find some way to mark off that user
-			//}
-			//else /*skip user*/
+	// Get members list
+	members, _ := s.GuildMembers(i.GuildID, "", 100)
+	// Get bot members
+	bots := []*dgo.Member{}
+	for _, member := range members {
+		if member.User.Bot {
+			bots = append(bots, member)
+		}
+	}
 	// Sends stats
-	utils.SendResponse("There are "+fmt.Sprint(MemberCount)+" many users "+fmt.Sprint(ApproximatePresenceCount)+" of which are online", i, s)
-	utils.SendResponse("There are "+fmt.Sprint(numberOfStaff)+" many users "+fmt.Sprint(numberOfStaffOnline)+" of which are online", i, s)
-	utils.SendResponse("There are "+fmt.Sprint(numberOfBots)+" many bots", i, s)
-	utils.SendResponse("There are "+fmt.Sprint(PremiumSubscriptionCount)+" current boosters", i, s)
+	utils.SendResponse("There are "+fmt.Sprint(len(members))+" many members", i, s)
+	utils.SendResponse("There are "+fmt.Sprint(len(bots))+" many bots", i, s)
 }
-
