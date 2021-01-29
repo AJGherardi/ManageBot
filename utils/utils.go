1ccf0860e2894d39d09e71f0b40fcbd469172de7
Alexander Gherardi <alexander.2003@live.com>
2021-01-29 15:35:35 -0500

    Reaction roles initial

--- utils/utils.go
+++ utils/utils.go
@@ -7,8 +7,9 @@ import (
 )
 
 // SendResponse sends a embed msg in the current channel
-func SendResponse(response string, i *dgo.InteractionCreate, s *dgo.Session) {
-	s.ChannelMessageSendEmbed(i.ChannelID, embed.NewGenericEmbed("", response))
+func SendResponse(response string, i *dgo.InteractionCreate, s *dgo.Session) *dgo.Message {
+	message, _ := s.ChannelMessageSendEmbed(i.ChannelID, embed.NewGenericEmbed("", response))
+	return message
 }
 
 // MatchSubcommand searches a list of subcommands for a matching handler for a / command
