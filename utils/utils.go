6337298f8050987cacfdbd98d2054078255edb00
Alexander Gherardi <alexander.2003@live.com>
2021-02-02 20:33:05 +0000

    add say dm

--- utils/utils.go
+++ utils/utils.go
@@ -6,6 +6,12 @@ import (
 	embed "github.com/clinet/discordgo-embed"
 )
 
+// SendDM sends a embed msg in the user dm
+func SendDM(response, channelID string, s *dgo.Session) *dgo.Message {
+	message, _ := s.ChannelMessageSendEmbed(channelID, embed.NewGenericEmbed("", response))
+	return message
+}
+
 // SendResponse sends a embed msg in the current channel
 func SendResponse(response string, i *dgo.InteractionCreate, s *dgo.Session) *dgo.Message {
 	message, _ := s.ChannelMessageSendEmbed(i.ChannelID, embed.NewGenericEmbed("", response))
